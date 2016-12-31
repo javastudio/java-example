bdde626f50db79de2b0c46789a2d162449d46480
Guntur Poetra <poetra.guntur@gmail.com>
2016-12-31 07:02:12 +0700

    lesson-1

---
+++ lesson-1/HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+ * Lesson - 1
+ *
+ * Display "hello, world" to console
+**/
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		// Menampilkan "hello, world" pada terminal / console
+		System.out.println("Hello, world");
+	}
+}
